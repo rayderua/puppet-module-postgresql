cd5f417bd103f2a14e67c743851684d62fc11fbb
Aleksey Opalev <reg@rayder.su>
2019-08-11 21:21:15 +0300

    -

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -6,7 +6,7 @@ class postgresql::configure {
 
 
   $version  = $postgresql::version;
-  $clusters = hiera('postgresql::clusters', {})
+  $clusters = hiera('postgresql::clusters', false)
 
   $default_pg_config  = $postgresql::params::config_postgresql_default;
   $default_pg_hba     = $postgresql::params::pg_gba_postgresql_default;
@@ -24,72 +24,76 @@ class postgresql::configure {
     }
   }
 
-  keys($clusters).each | $cluster | {
-    # add version/cluster variables to config
-    $path_config = {
-      'data_directory'    => "/var/lib/postgresql/${version}/${cluster}",
-      'hba_file'          => "/etc/postgresql/${version}/${cluster}/pg_hba.conf",
-      'ident_file'        => "/etc/postgresql/${version}/${cluster}/pg_ident.conf",
-      'external_pid_file' => "/run/postgresql/${version}-${cluster}.pid",
-    }
+  if ( $clusters == false ) {
+    notify{"No clusters configured yet": }
+  } else {
+    keys($clusters).each | $cluster | {
+      # add version/cluster variables to config
+      $path_config = {
+        'data_directory'    => "/var/lib/postgresql/${version}/${cluster}",
+        'hba_file'          => "/etc/postgresql/${version}/${cluster}/pg_hba.conf",
+        'ident_file'        => "/etc/postgresql/${version}/${cluster}/pg_ident.conf",
+        'external_pid_file' => "/run/postgresql/${version}-${cluster}.pid",
+      }
 
-    $default_config = deep_merge($_defaul_config, $path_config);
+      $default_config = deep_merge($_defaul_config, $path_config);
 
-    # add/replace user defined config
-    $user_config = hiera("postgresql::clusters::${cluster}::config", {})
+      # add/replace user defined config
+      $user_config = hiera("postgresql::clusters::${cluster}::config", {})
 
-    $pg_config  = deep_merge($default_config, $user_config)
-    $pg_hba     = hiera("postgresql::clusters::${cluster}::pg_hba", {})
-    $pg_ident   = hiera("postgresql::clusters::${cluster}::pg_ident", {})
+      $pg_config = deep_merge($default_config, $user_config)
+      $pg_hba = hiera("postgresql::clusters::${cluster}::pg_hba", {})
+      $pg_ident = hiera("postgresql::clusters::${cluster}::pg_ident", {})
 
-    # Create cluster
-    exec { "postgresql create cluster ${version}/${cluster}":
-      command => "pg_createcluster ${version} ${cluster}",
-      onlyif  => "pg_ctlcluster ${version} ${cluster} status 2>&1 | grep 'not exist'",
-      path    => ['/bin', '/usr/bin'],
-      require => [ $::postgresql::install::packages]
-    } ->
+      # Create cluster
+      exec { "postgresql create cluster ${version}/${cluster}":
+        command => "pg_createcluster ${version} ${cluster}",
+        onlyif  => "pg_ctlcluster ${version} ${cluster} status 2>&1 | grep 'not exist'",
+        path    => ['/bin', '/usr/bin'],
+        require => [ $::postgresql::install::packages]
+      } ->
 
-    # Config directory
-    file { ["/etc/postgresql/${version}/${cluster}"]:
-      ensure  => directory,
-      owner   => 'postgres',
-      group   => 'postgres',
-      mode    => '0755',
-      require => [ File["/etc/postgresql/${version}"], Exec["postgresql create cluster ${version}/${cluster}"]]
-    } ->
+      # Config directory
+      file { ["/etc/postgresql/${version}/${cluster}"]:
+        ensure  => directory,
+        owner   => 'postgres',
+        group   => 'postgres',
+        mode    => '0755',
+        require => [ File["/etc/postgresql/${version}"], Exec["postgresql create cluster ${version}/${cluster}"]]
+      } ->
 
-    # Data directory
-    file { ["/var/lib/postgresql/${version}/${cluster}"]:
-      ensure => directory,
-      owner  => 'postgres',
-      group  => 'postgres',
-      mode   => '0700',
-    } ->
+      # Data directory
+      file { ["/var/lib/postgresql/${version}/${cluster}"]:
+        ensure => directory,
+        owner  => 'postgres',
+        group  => 'postgres',
+        mode   => '0700',
+      } ->
 
-    file { "/etc/postgresql/${version}/${cluster}/postgresql.conf":
-      owner   => 'postgres',
-      group   => 'postgres',
-      mode    => '0644',
-      content => template("postgresql/postgres.conf.erb"),
-      notify  => Exec["postgresql reload ${version}/${cluster}"]
-    } ->
+      file { "/etc/postgresql/${version}/${cluster}/postgresql.conf":
+        owner   => 'postgres',
+        group   => 'postgres',
+        mode    => '0644',
+        content => template("postgresql/postgres.conf.erb"),
+        notify  => Exec["postgresql reload ${version}/${cluster}"]
+      } ->
 
-    file { "/etc/postgresql/${version}/${cluster}/pg_hba.conf":
-      owner   => 'postgres', group => 'postgres', mode => '0644',
-      content => template("puppet:///modules/postgresql/pg_hba.conf.erb"),
-      notify  => Exec["postgresql reload ${version}/${cluster}"]
-    } ->
+      file { "/etc/postgresql/${version}/${cluster}/pg_hba.conf":
+        owner   => 'postgres', group => 'postgres', mode => '0644',
+        content => template("puppet:///modules/postgresql/pg_hba.conf.erb"),
+        notify  => Exec["postgresql reload ${version}/${cluster}"]
+      } ->
 
-    file { "/etc/postgresql/${version}/${cluster}/pg_ident.conf":
-      owner  => 'postgres', group => 'postgres', mode => '0644',
-      content => template("puppet:///modules/postgresql/pg_ident.conf.erb"),
-      notify => Exec["postgresql reload ${version}/${cluster}"]
-    }
+      file { "/etc/postgresql/${version}/${cluster}/pg_ident.conf":
+        owner   => 'postgres', group => 'postgres', mode => '0644',
+        content => template("puppet:///modules/postgresql/pg_ident.conf.erb"),
+        notify  => Exec["postgresql reload ${version}/${cluster}"]
+      }
 
-    exec { "postgresql reload ${version}/${cluster}":
-      command     => "/usr/bin/pg_ctlcluster ${version} ${cluster} reload",
-      refreshonly => true
+      exec { "postgresql reload ${version}/${cluster}":
+        command     => "/usr/bin/pg_ctlcluster ${version} ${cluster} reload",
+        refreshonly => true
+      }
     }
   }
 }
